cef0472d355ec62dd5bb167206796517e9f2cebb
ebube <ebubeagwaze@gmail.com>
2023-04-05 15:23:12 +0100

    webpack changes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
     appDir: true,
   },
   productionBrowserSourceMaps: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        default: false,
+      };
+      config.optimization.runtimeChunk = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
